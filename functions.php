75a70259ede9bc3280c3af55bd65ab8ca4d86d12
Tulio Vieira <tulio.vieira-x@unqork.com>
2023-11-28 02:05:14 -0300

    a semi working version

--- functions.php
+++ functions.php
@@ -25,21 +25,25 @@ add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
 
 function register_hallura_styles_and_scripts() {
     if ( is_page_template( 'page_hallura_front_page.php' ) ) {
+        wp_enqueue_style('hallura-bootstrap', get_stylesheet_directory_uri() . "/assets/css/bootstrap.min.css");
+        
         wp_enqueue_style( 'hallura-core', get_stylesheet_directory_uri() . '/assets/css/master.css', [] );
+        wp_enqueue_style( 'hallura-front-page', get_stylesheet_directory_uri() . '/hallura-front-page.css', [] );
+        wp_enqueue_style( 'hallura-animate', get_stylesheet_directory_uri() . '/assets2/animate.min.css', [] );
+        wp_enqueue_script( 'hallura-wow', get_stylesheet_directory_uri() . '/assets2/wow.min.js', [] );
         wp_enqueue_style( 'hallura-revolution-settings', get_stylesheet_directory_uri() . '/revolution/css/settings.css', [] );
         wp_enqueue_style( 'hallura-revolution-layers', get_stylesheet_directory_uri() . '/revolution/css/layers.css', [] );
-        wp_enqueue_style( 'hallura-revolution-nav', get_stylesheet_directory_uri() . '/revolution/css/navigation.css', [] );
-
-        wp_enqueue_style( 'hallura-bootstrap', get_stylesheet_directory_uri() . '/assets/css/new/bootstrap.min.css', [] );
+        
         wp_enqueue_style( 'hallura-default', get_stylesheet_directory_uri() . '/assets/css/new/default.css', [] );
         wp_enqueue_style( 'hallura-style', get_stylesheet_directory_uri() . '/assets/css/new/style.css', [] );
-
+        
         wp_enqueue_style( 'hallura-owl', '//cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css', [] );
         wp_enqueue_script( 'hallura-jquery-3.6', '//code.jquery.com/jquery-3.6.0.min.js', [] );
         wp_enqueue_script( 'hallura-owl-script', '//cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js', ['hallura-jquery-3.6'] );
-
+        
         # footer scripts
-        wp_enqueue_script( 'hallura-main-js', get_stylesheet_directory_uri() . '/assets/js/main.js', [], false, true );
+        // wp_enqueue_script( 'hallura-main-js', get_stylesheet_directory_uri() . '/assets/js/main.js', [], false, true );
+        wp_enqueue_script( 'hallura-js', get_stylesheet_directory_uri() . '/assets2/main.js', [], false, true );
         wp_enqueue_script( 'hallura-jquery', get_stylesheet_directory_uri() . '/assets/js/jquery.min.js', [], false, true );
         wp_enqueue_script( 'hallura-smoothscroll', get_stylesheet_directory_uri() . '/assets/js/smoothscroll.js', ['hallura-jquery'], false, true );
         wp_enqueue_script( 'hallura-js-plugins', get_stylesheet_directory_uri() . '/assets/js/plugins.js', ['hallura-jquery'], false, true );
@@ -47,4 +51,4 @@ function register_hallura_styles_and_scripts() {
         wp_enqueue_script( 'hallura-kit-fontawesome', '//kit.fontawesome.com/59791f4fda.js', ['hallura-jquery'], false, true );
     }
 }
-add_action( 'wp_enqueue_scripts', 'register_hallura_styles_and_scripts');
+add_action( 'wp_enqueue_scripts', 'register_hallura_styles_and_scripts', 1);
--- page_hallura_front_page.php
+++ page_hallura_front_page.php
@@ -6,8 +6,9 @@
 get_header(); ?>
 
 <main id="main" class="site-main" role="main">
+
   <!--== Who We Are Start ==-->
-  <section class="white-bg xs-pb-100" id="about" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/bg-text.jpg' ?>'); background-size: cover;">
+  <section class="hallura white-bg xs-pb-100" id="about" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/bg-text.jpg' ?>'); background-size: cover;">
     <div class="container">
       <div class="row">
         <div class="col-md-8 centerize-col text-center">
@@ -24,8 +25,111 @@ get_header(); ?>
       </div>
     </div>
   </section>
-  <h1>This is a test</h1>
+  <!--== Who We Are End ==-->
 
+  <!--== About Company Start ==-->
+  <section class="hallura grey-bg xs-pb-60">
+    <div class="col-md-6 col-sm-6 bg-flex bg-flex-left">
+      <div class="bg-flex-holder bg-flex-cover" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/assets/images/gel.jpg' ?>);">
+        <div class="video-box_overlay">
+          <div class="center-layout">
+            <div class="v-align-middle"> <a class="popup-youtube" href="https://www.youtube.com/watch?v=jiAt3mdHctw">
+                <div class="play-button"><i class="tr-icon ion-android-arrow-dropright"></i></div>
+              </a> </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div class="col-md-6 col-sm-6 col-md-offset-6 col-sm-offset-6">
+        <div class="col-inner spacer text-left">
+          <h1 class="raleway-font mt-0 font-50px font-600 xs-font-27px">How does it work?
+          </h1>
+          <p class="mt-30 intro">Hallura's BioLinkMatrix, a groundbreaking advancement in dermal filler technology, is transforming the landscape of hyaluronic acid (HA) cross-linking.
+              <br>
+                <br>
+            This innovative technology from HalluraTM meticulously preserves the intricate structure of extended HA polymer chains. The outcome is the creation of an unparalleled level of PURE ELASTICITY, a distinctive quality that truly distinguishes it from other competitors in the market. 
+              <br>
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--== About Company End ==-->
+
+  <!--== Results Start ==-->
+  <section class="hallura hallura-results parallax-bg fixed-bg" data-parallax-bg-image="<?php echo get_stylesheet_directory_uri() . '/assets/images/mulherr.png' ?>"
+    data-parallax-speed="0.5" data-parallax-direction="up">
+    <source src="<?php echo get_stylesheet_directory_uri() . '/assets/images/mulherr.png' ?>" type="image/png">
+    <div class="overlay-bg"></div>
+    <div class="section-title">
+      <h1 class="raleway-font white-color">Amazing results</h1>
+      <hr class="center_line default-bg">
+    </div>
+    <div class="hallura-results-container">
+      <div class="hallura-result">
+        <div class="wow fadeInRight white-color" data-wow-delay="0.1s">
+          <i class="fa-solid fa-arrow-up-short-wide font-60px"></i>
+          <h2><span class="counter font-600">104</span>%</h2>
+          <h3 class="font-200">Increase of elastin formation.</h3>
+        </div>
+      </div>
+      <div class="hallura-result">
+        <div class="wow fadeInRight white-color" data-wow-delay="0.2s">
+          <i class="fa-regular fa-clock font-60px"></i>
+          <h2><span class="counter font-600">88</span>%</h2>
+          <h3 class="dark-color font-200">Patients see improvement during the treatment</h3>
+        </div>
+      </div>
+      <div class="hallura-result">
+        <div class="wow fadeInRight white-color" data-wow-delay="0.3s">
+          <i class="fa-solid fa-star font-70px"></i>
+          <h2><span class="counter font-600">100</span>%</h2>
+          <h3 class="dark-color font-200">Excellent Tolerance</h3>
+        </div>
+      </div>
+      <div class="hallura-result">
+        <div class="wow fadeInRight white-color" data-wow-delay="0.4s">
+          <i class="fa-solid fa-leaf font-70px"></i>
+          <h2><span class="counter font-600">99</span>%</h2>
+          <h3 class="dark-color font-200">Naturality</h3>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--== Results End ==-->
+
+  <section class="white-bg xs-pb-100" id="about">
+    <div class="section-title">
+      <h2 class="raleway-font default-color"></h2>
+      <h1 class="raleway-font">What makes Hallura™ <br>the ultimate
+        choice</h1>
+    </div>
+    <div class="hallura-features">
+      <div class="feature-box text-center xs-mb-30 feature-box-default-bg center-feature">
+        <i class="fa-solid fa-leaf font-40px"></i>
+        <h4 class="mt-0 font-600">Natural</h4>
+        <p class="font-400">With about 99% HA in its natural state and up to 4 times less HA content,
+          Hallura is the most natural HA filler.</p>
+        <br>
+      </div>
+      <div class="feature-box text-center xs-mb-30 feature-box-default-bg center-feature">
+        <i class="fa-solid fa-check font-40px"></i>
+        <h4 class="mt-0 font-600">Efficient</h4>
+        <p class="font-400">Unique elastic properties enable superior intrinsic lifting force. Hallura gels
+          promote collagen and elastin formation for long-lasting natural results.</p>
+      </div>
+      <div class="feature-box text-center feature-box-default-bg center-feature">
+        <i class="fa-solid fa-lock font-40px"></i>
+        <h4 class="mt-0 font-600">Safe</h4>
+        <p class="font-400">Hallura’s ultra-pure HA integrates naturally, homogenously and seamlessly into
+          the tissue.
+          <br><br>
+        </p>
+      </div>
+    </div>
+  </section>
+  <h1>end</h1>
 
 </main><!-- #main -->
 
