2571b6864d6ebde5a98104b02aad6e000a918e32
Tulio Vieira <tulio.vieira-x@unqork.com>
2023-12-13 23:20:12 -0300

    increase hallura-stamp size

--- page_hallura_front_page.php
+++ page_hallura_front_page.php
@@ -28,13 +28,13 @@ get_header(); ?>
 
 <main id="main" class="site-main" role="main">
     <!--== Who We Are Start ==-->
-    <section class="white-bg xs-pb-100" id="about" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/bg-text.jpg' ?>'); background-size: cover;">
+    <section class="white-bg xs-pb-100" id="about" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/bg-text.jpg' ?>'); background-size: cover;     background-position: center;">
       <div class="container">
         <div class="row">
           <div class="col-md-8 centerize-col text-center">
             <div class="section-title who-we-are">
               <h2 class="raleway-font default-color"></h2>
-              <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/hallura-stamp.png' ?>" alt="" width="15%">
+              <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/hallura-stamp.png' ?>" alt="" width="50%">
               <h3 class="raleway-font">As we move towards natural beauty, Hallura™ developed the next generation of HA fillers, providing the ultimate choice empowering YOU at your Natural, Beautiful and Confident self.</h3>
               <br>
               <h3 class="raleway-font">Hallura’s fillers are based on breakthrough BioLinkMatrix technology which provides an adaptive elasticity gel that induces optimal integration to your skin, bio-stimulating collagen and elastin production for long-lasting results.</h3>
